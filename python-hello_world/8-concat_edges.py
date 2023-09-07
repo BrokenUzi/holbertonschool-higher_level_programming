aa45978938aab49dd33828057cc295e29f1984e2
BrokenUzi <jcup143@gmail.com>
2023-09-07 15:09:27 -0700

    Played around with fstrings and concatinations. Then concated file 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
         language that combines remarkable power with very clear syntax"
-        print(str)
+print(str[39:66] + " " + str[114:118] + " " + str[:6])
