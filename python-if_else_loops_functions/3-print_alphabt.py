ba76754f08d4ce74cafcacd5215e3d9bacbdda3d
BrokenUzi <jcup143@gmail.com>
2023-09-07 19:45:42 -0700

    Used same code as number 2. I couldnt think of another way besides slicing to accomplish this. Theres a for loop, if, and else. The checker better like this one or else

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if chr(i) == 'e' or chr(i) == 'q':
-        continue
-    print("{}".format(chr(i)), end="")
+for x in range(ord('a'), ord('z') + 1):
+    print("{}".format(chr(x)), end='') if chr(x) not in 'qe' else None
