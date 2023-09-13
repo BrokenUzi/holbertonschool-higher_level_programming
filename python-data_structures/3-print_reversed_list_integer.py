272ee7cbfd069e07cfb3da35a75ee8c758540dc9
BrokenUzi <jcup143@gmail.com>
2023-09-12 17:47:07 -0700

    Hopefully this will clear some checks

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-	for x in range(len(my_list) - 1, -1, -1):
-		print("{}".format(my_list[x]))
+	main_length = len(my_list)
+
+	new_new = my_list[:]
+
+	if 0 <= idx <main_length:
+		new_new[idx] = element
+
+	return(new_new)
