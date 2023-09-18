282dab6159bcf6210cbdba3e180a650fbb7483a4
BrokenUzi <jcup143@gmail.com>
2023-09-17 20:44:22 -0700

    Indentation fixes

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,12 +5,11 @@ class Square:
 	def __init__(self, size=0):
 		"size (int):"
 
-	if not isinstance(size, int):
-		"Checks if size value is an int. If its not an int Type Error occurs demainding an int"
-		raise TypeError("size must be an integer")
-	elif size <= 0:
-		"Checks if size int is greater than 0. It'll raise a type error if not > 0"
-		raise TypeError("size must be >= 0")
-
-self._size = size
-"Sets size of square"
+		if not isinstance(size, int):
+			"Checks if size value is an int. If its not an int Type Error occurs demainding an int"
+			raise TypeError("size must be an integer")
+		elif size <= 0:
+			"Checks if size int is greater than 0. It'll raise a type error if not > 0"
+			raise TypeError("size must be >= 0")
+		self._size = size
+		"Sets size of square"
