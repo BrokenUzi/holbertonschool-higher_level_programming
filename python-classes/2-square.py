162042e94876f2c785d942d7c372af10b3b7eb06
BrokenUzi <jcup143@gmail.com>
2023-09-17 21:32:25 -0700

    Made size a private variable

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 class Square:
-	"Square is being represented"
+	""" Square is being represented """
 
-	def __init__(self, size=0):
-		"size (int):"
-		if not isinstance(size, int):
+	def __init__(self, __size=0):
+		""" size (int): """
+		if not isinstance(__size, int):
 			"Checks if size value is an int. If its not an int Type Error occurs demainding an int"
 			raise TypeError("size must be an integer")
-		elif size <= 0:
+		elif __size <= 0:
 			"Checks if size int is greater than 0. It'll raise a type error if not > 0"
 			raise TypeError("size must be >= 0")
-		self._size = size
+		self.__size = __size
