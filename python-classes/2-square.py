16cc171de6c5240f23b10b7ece0489cb3033a3a8
BrokenUzi <jcup143@gmail.com>
2023-09-17 20:29:04 -0700

    That didnt work reverted back to old

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
+"Define"
+
 class Square:
-    "Square is being represented"
+	"Square is being represented"
 
-    def __init__(self, size):
-        "size (int):"
-        if not isinstance(size, int):
-            print("size must be an integer")
-        elif size <= 0:
-            print("size must be >= 0")
-        else:
-            self.__size = size
-
-my_square = Square(5)
-
+	def __init__(self, size):
+		"size (int):"
+		self.__size = size
