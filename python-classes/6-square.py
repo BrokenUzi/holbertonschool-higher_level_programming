c9906861c3bfa428bb9d7b4b6484991f639a2096
BrokenUzi <jcup143@gmail.com>
2023-09-18 19:12:40 -0700

    It compiled!!

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -21,11 +21,15 @@ class Square:
 			raise ValueError("size must be >= 0")
 		self.__size = value
 
+	@property
+	def position(self):
+		return self.__position
+
 	@position.setter
 	def position(self, value):
 		if not isinstance(value, tuple) or len(value) != 2:
 			raise TypeError("position must be a tuple of 2 positive integers")
-		if not all(isinstance(item in value):
+		if not all(isinstance(item, int) for item in value):
 			raise TypeError("position must be a tuple of 2 positive integers")
 		if not all(item >= 0 for item in value):
 			raise TypeError("position must be a tuple of 2 positive integers")
