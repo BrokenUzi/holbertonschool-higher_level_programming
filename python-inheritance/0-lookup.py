2dada767856e963553c6b460117073f6069475da
BrokenUzi <jcup143@gmail.com>
2023-09-25 19:36:38 -0700

    Mayve

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -2,3 +2,12 @@
 def lookup(obj):
 	""" Returns a list of available attributes """
 	return [item for item in dir(obj) if not callable(getattr(obj, item))]
+
+class Object:
+	def __init__(self):
+		self.attribute1 = 42
+
+obj = Object()
+
+attributes_and_methods = lookup(obj)
+print(attributes_and_methods)
