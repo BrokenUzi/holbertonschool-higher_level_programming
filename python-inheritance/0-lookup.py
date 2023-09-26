947dc9dcf9dbc3d9ca678edbd6a263b005541e98
BrokenUzi <jcup143@gmail.com>
2023-09-25 19:55:28 -0700

    Lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python3
-	""" Returns a lost of available attributes and methods """
+""" Returns a lost of available attributes and methods """
 
 def lookup(obj):
-	""" Returns a list of available attributes and methods """
-	return dir(obj)
+	""" Gets list of available attributes and methods """
+	all_attributes_and_methods = dir(obj)
+
+	""" Filters out callable methods from list """
+	attributes = [item for item in all_attributes_and_methods if not callable(getattr(obj, item))]
+
+	return attributes
+
+class Lst:
+	def __init__(self):
+		self.attribute1 = 42
+
+	def method1(self):
+		pass
+
+obj = Lst()
+
+attributes_and_methods = lookup(obj)
+print(attributes_and_methods)
