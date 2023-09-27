d92906468a4dab97d033792fe76affa5ecc7e0fd
BrokenUzi <jcup143@gmail.com>
2023-09-26 18:16:56 -0700

    Test

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
-""" is_same_class checks if its the same class """
+""" Tests if input is a_class """
 
 def is_same_class(obj, a_class):
-	""" Checks if its the same class """
-	return type(obj) is a_class
-
-class C1:
-	pass
-
-obj1 = C1()
-obj2 = "Yo yo yo"
-
-""" True """
-print(is_same_class(obj1, C1))
-""" True """
-print(is_same_class(obj2, str))
-""" False """
-print(is_same_class(obj2, int))
+	""" Tests if input is a_class """
+	if type(obj) is a_class:
+		return True
+	else:
+		return False
