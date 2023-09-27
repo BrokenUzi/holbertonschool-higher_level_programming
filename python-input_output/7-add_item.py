4ce0751dc4ab9b9d8dc33e98e2da998ba44676cf
BrokenUzi <jcup143@gmail.com>
2023-09-26 21:07:23 -0700

    Starting to get difficult

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""  """
+""" Adds args as a list then saves to a file """
 
 import sys
 import json
@@ -14,3 +14,10 @@ def load_from_json_file(filename):
 	return data
 
 arguments = sys.argv[1:]
+
+try:
+	existing_list = load_from_json_file('add_item.json')
+except FileNotFoundError:
+	existing_list = []
+
+save_to_json_file(existing_list, 'add_item.json')
