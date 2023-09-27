515c284c9ecc452dd9614142e65fb7d60544a2b2
BrokenUzi <jcup143@gmail.com>
2023-09-26 21:17:07 -0700

    Simplified the code

--- 9-student.py
+++ 9-student.py
@@ -2,15 +2,13 @@
 """ Defines a Student """
 
 class Student:
+	""" Student Class with first and last name with age attributes """
+	pass
+
 	def __init__(self, first_name, last_name, age):
 		self.first_name = first_name
 		self.last_name = last_name
 		self.age = age
 
 	def to_json(self):
-		""" Returns Students Instance """
-		return {
-			'first_name': self.first_name,
-			'last_name': self.last_name,
-			'age': self.age
-		}
+		return self.__dict__
