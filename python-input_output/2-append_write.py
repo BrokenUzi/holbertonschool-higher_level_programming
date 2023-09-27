52a2d517f4d74f4aef646f6e40d7ef061547f28e
BrokenUzi <jcup143@gmail.com>
2023-09-26 20:51:17 -0700

    Typo

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,6 +3,6 @@
 
 def append_write(filename="", text=""):
 	""" Appends a string to the end of the UTF-8 file and returns # of chars that were implemented """
-	with open(filename, 'a', encoding="utf-8") as file:
-		chars_added = file.write(text)
+	with open(filename, 'a', encoding="utf-8") as NotAFileWinkWink:
+		chars_added = NotAFileWinkWink.write(text)
 	return chars_added
