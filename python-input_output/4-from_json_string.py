aa332cc4dc85ccbf111b71557a7e477874701633
BrokenUzi <jcup143@gmail.com>
2023-09-26 20:56:26 -0700

    Indentation

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -2,6 +2,7 @@
 """ Returns a py obj as a json str """
 
 import json
-	def from_json_string(my_str):
+
+def from_json_string(my_str):
 	""" Returns a py obj as a json str """
 	return json.loads(my_str)
